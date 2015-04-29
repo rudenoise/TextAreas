b410f7ed7e8a70a118dbb2bae64556eae0b8d67b
Joel Hughes <joel@redjumper.net>
2015-04-29 11:40:06 +0100

    make text area full screen

--- TextAreas/TextAreas.Shared/js/textArea.js
+++ TextAreas/TextAreas.Shared/js/textArea.js
@@ -3,18 +3,22 @@
     var goBtn, textArea, output;
 
     function getContent() {
+        var innerHTML;
         // swap line breaks for <br>
         // in real app also needs escaping of tags etc... (is there anything built in?)
         // also in app the area would need to grow to accomodate pasted/typed input without using scroll-bars
-        output.innerHTML = textArea.value.replace(/[\r\n]{1,}/g, '<br></br>');
+        innerHTML = textArea.value.replace(/<[^>]*>/g, '');
+        output.innerHTML = innerHTML.replace(/[\r\n]{1,}/g, '<br></br>');
     }
 
     WinJS.Namespace.define('textArea', {
         setUp: function () {
+            var width;
             goBtn = document.getElementById('textAreaGo');
             textArea = document.getElementById('textAreaIn');
             output = document.getElementById('textAreaOut');
-
+            width = textArea.parentElement.clientWidth;
+            textArea.style.paddingRight = String(width - 525) + 'px';
             goBtn.addEventListener('pointerdown', getContent, false);
         }
     });
